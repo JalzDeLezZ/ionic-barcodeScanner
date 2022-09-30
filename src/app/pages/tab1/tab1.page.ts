f694db02e602aef698855297f4f1ed1698f5f8a0
James Jalz Díaz López <j4mes.delez@gmail.com>
2022-09-29 20:23:03 -0500

    implements barcode scanner

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(private barcodeScanner: BarcodeScanner) {}
+  swiperOpts = {
+    allowSlidePrev: false,
+    allowSlideNext: false,
+  };
 
-  constructor() {}
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter');
+    this.mScan();
+  }
 
+  mScan() {
+    this.barcodeScanner
+      .scan()
+      .then((barcodeData) => {
+        console.log('Barcode data', barcodeData);
+      })
+      .catch((err) => {
+        console.log('Error', err);
+      });
+  }
 }
